24f36b789e441346420c8396069550fe77f3542b
noah1984 <noahallen1984@hotmail.com>
2016-12-11 09:00:41 -0800

    Another very minor update to headers of code files

--- FTP-RC/Encryption.cs
+++ FTP-RC/Encryption.cs
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 Noah Allen
-// This file is part of FTP-RC
+//
+// This file is part of FTP-RC.
 //
 // FTP-RC is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
